b90f789dcc97e8c4a431fba5e6fb12897a80cce3
vi2233m <31640283+vi2233m@users.noreply.github.com>
2019-01-15 00:01:47 +0800

    second

--- src/main/java/page/OptionalPage.java
+++ src/main/java/page/OptionalPage.java
@@ -1,24 +1,34 @@
 package page;
 
+import driver.Driver;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.touch.TouchActions;
 
 public class OptionalPage extends BasePage{
     private By addId = locate("id/image");
-
+    private By searchId = locate("id/action_create_cube");
+    private By stockXpah = By.xpath("//*[contains(@resource-id, 'id/listview')]/android.widget.FrameLayout[1]/android.widget.LinearLayout/android.widget.LinearLayout[1]");
 
     public SearchPage add(){
-
+        find(addId).click();
         return new SearchPage();
     }
 
-    public SearchPage addAgain(){
-
+    public SearchPage gotoSearch(){
+        find(searchId).click();
         return new SearchPage();
     }
 
-    public OptionalPage remove(){
+    public StockEditPage longPress(){
+        TouchActions ta = new TouchActions(Driver.getCurrentDriver());
+        AndroidElement el = (AndroidElement)find(stockXpah);
+        ta.longPress(el).wait(10000).perform();//长按10s
 
-        return this;
     }
+
 
 }
---
+++ src/main/java/page/StockEditPage.java
@@ -0,0 +1,9 @@
+package page;
+
+public class StockEditPage extends BasePage{
+
+    public OptionalPage remove(){
+
+        return new OptionalPage();
+    }
+}
